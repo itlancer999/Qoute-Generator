07dc5bef311adbd06a88413f2c0f7427bb5818ce
itlancer999 <it.lancer999@gmail.com>
2021-01-18 15:21:03 -0500

    adding files

---
+++ script.js
@@ -0,0 +1,58 @@
+const qoutes = [
+  {
+    qoute: "The most decisive actions of our life... are most often unconsidered actions.",
+    author: "André Gide"
+  },
+  {
+    qoute: "Living at risk is jumping off the cliff and building your wings on the way down.",
+    author: "Ray Bradbury"
+  },
+  {
+    qoute: "Happiness is when what you think, what you say, and what you do are in harmony.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    qoute: "An eye for an eye only ends up making the whole world blind.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    qoute: "You must be the change you wish to see in the world.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    qoute: "Failure will never overtake me if my determination to succeed is strong enough.",
+    author: "Og Mandino"
+  },
+  {
+    qoute: "Strength does not come from physical capacity. It comes from an indomitable will.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    qoute: "In order to succeed, we must first believe that we can.",
+    author: "Nikos Kazantzakis"
+  },
+  {
+    qoute: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius"
+  },
+  {
+    qoute: "Choose a job you love, and you will never have to work a day in your life.",
+    author: "Confucius"
+  },
+  {
+    qoute: "Your talent is God's gift to you. What you do with it is your gift back to God.",
+    author: "Leo Buscaglia"
+  },
+  {
+    qoute: "Problems are not stop signs, they are guidelines.",
+    author: "Robert H. Schuller"
+  },
+  {
+    qoute: "Start where you are. Use what you have. Do what you can.",
+    author: "Arthur Ashe"
+  },
+  {
+    qoute: "Knowing is not enough, we must apply. Willing is not enough, we must do.",
+    author: "Johann Wolfgang von Goethe"
+  }
+];
